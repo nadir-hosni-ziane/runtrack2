37cf4b8a638552b4ac7e24786ecf4b6d7298a7f5
nadir-ziane <nadir.ziane@laplateforme.io>
2020-11-12 15:24:34 +0100

    jour09 job12

---
+++ jour09/job12/index.php
@@ -0,0 +1,46 @@
+<?php
+$sql = mysqli_connect('localhost', 'root' , '', 'jour08');
+$request = mysqli_query($sql, "SELECT `prenom`,`nom`,`naissance` FROM etudiants WHERE `naissance` BETWEEN '1998-01-01' AND '2018-01-01'");
+$resultat = mysqli_num_rows($request);
+//var_dump($resultat); 
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <style>
+        table {
+        border-collapse: collapse;
+        }
+        th {
+            border: 1px solid black;
+            padding: 3px
+        }
+        td {
+            border: 1px solid black;
+            padding: 3px
+        }
+    </style>
+</head>
+
+<body>
+<table>
+<thead>
+<tr>
+<td>prénom</td>
+<td>nom</td>
+<td>naissance</td>
+<tbody>
+<?php
+    while ($row = mysqli_fetch_array($request)) {
+            echo "<tr><td>$row[prenom]</td>";
+            echo "<td>$row[nom]</td>";
+            echo "<td>$row[naissance]</td></tr>";
+    }
+?>
+</tbody>
+</table>
+</body>
